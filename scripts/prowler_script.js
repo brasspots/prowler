1d9e0c65d4ef3a5666957862b0f4a4e7a8f66bfe
brasspots <44511851+brasspots@users.noreply.github.com>
2018-10-28 11:40:03 +0000

    Update prowler_script.js
    Added traverse and string_check functions
    currently only alerts user to number of matches

--- scripts/prowler_script.js
+++ scripts/prowler_script.js
@@ -1,3 +1,47 @@
-// code to run on page load goes here
+// TODO: load in bad words
+bad_words = ['iceberg', 'penguin'];
+// initialise matches list and match_count
+matches = [];
+match_count = 0;
+// TODO: check given string for mathces with bad_words
+function string_check(text) {
+  // initialise hit flag
+  hit = false;
+  for (let i = 0; i < bad_words.length; i++) {
+    // search for a match
+    if (text.includes(bad_words[i])) {
+      // set hit and increment match_count
+      hit = true;
+      match_count++
+    }
+  };
+  // return if there has been a match
+  return hit
+};
+// recursive function to traverse DOM and get to end nodes
+function traverse(element){
+  // end of tree
+  if (element.childElementCount === 0) {
+    // search alt if element is img
+    if (element.tagName === 'IMG') {
+      if (string_check(element.alt)) {
+        matches.push(element)
+      }
+    // search innerText if innerText is not blank
+    } else if (element.innerText !== '') {
+      if (string_check(element.innerText)) {
+        matches.push(element)
+      }
+    }
+  // recurse funcrion on all elements
+  } else {
+    for (let i = 0; i < element.childElementCount; i++) {
+      traverse(element.children[i])
+    }
+  }
+}
 
-console.log("Prowler script has run")
+// main code
+
+traverse(document.body)
+alert(match_count)
