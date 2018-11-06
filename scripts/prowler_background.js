426064a28b2770cda50f59cf7a1a8ae12223321b
brasspots <44511851+brasspots@users.noreply.github.com>
2018-11-06 11:55:56 +0000

    Update prowler_background.js
    Fixed trigger call

--- scripts/prowler_background.js
+++ scripts/prowler_background.js
@@ -1,5 +1,5 @@
 // add a listener to page load
-chrome.webRequest.onCompleted.addListener(on_load);
+chrome.webRequest.onCompleted.addListener(on_load, {'urls': ['*://*/*']}, []);
 
 // define trigger on load
 function on_load() {
