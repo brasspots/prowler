24c81bd3ab296700a8333d9a9cf54be1bb8e56d3
brasspots <44511851+brasspots@users.noreply.github.com>
2018-11-06 11:25:39 +0000

    Update prowler_background.js
    Fixed silly errors

--- scripts/prowler_background.js
+++ scripts/prowler_background.js
@@ -1,5 +1,5 @@
 // add a listener to page load
-lookup chrom.webRequest.onCompleted.addListener(on_load);
+chrome.webRequest.onCompleted.addListener(on_load);
 
 // define trigger on load
 function on_load() {
