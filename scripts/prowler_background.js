2470cedc35441a431b724c9108a7c033d546afba
brasspots <44511851+brasspots@users.noreply.github.com>
2018-11-06 11:21:06 +0000

    Update prowler_background.js
    Basic listener to work out behaviour

--- scripts/prowler_background.js
+++ scripts/prowler_background.js
@@ -1,1 +1,7 @@
-// lookup chrom.webRequest.onCompleted.addListener
+// add a listener to page load
+lookup chrom.webRequest.onCompleted.addListener(on_load);
+
+// define trigger on load
+function on_load() {
+  console.log('Page loaded')
+}
