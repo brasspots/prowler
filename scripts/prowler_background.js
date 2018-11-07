f25c2254419a0083083810d8f4428b0ac3129226
brasspots <44511851+brasspots@users.noreply.github.com>
2018-11-07 21:37:53 +0000

    Update prowler_background.js
    Added debugging statments
    added missing semi-colons

--- scripts/prowler_background.js
+++ scripts/prowler_background.js
@@ -1,17 +1,21 @@
 // get starting time
-last_request_time = (new Date()).getTime()
+last_request_time = (new Date()).getTime();
 // add a listener to page load
 chrome.webRequest.onCompleted.addListener(on_load, {'urls': ['*://*/*']}, []);
 
 // define trigger on load
 function on_load() {
+  // debugging
+  console.log("Prowler: on_load called");
   // get current time
-  current_request_time = (new Date()).getTime()
+  current_request_time = (new Date()).getTime();
   time_dif = current_request_time - last_request_time
   // update last reuest time
   last_request_time = current_request_time
   // check for suitable time dif
   if (time_dif > 50) {
+    // debugging
+    console.log("Prowler: message sent");
     // send message to active tab
     chrome.tabs.query({active: true, currentWindow: true}, function(tab_list) {
       // send message
