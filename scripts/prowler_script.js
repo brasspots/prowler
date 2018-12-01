1f9dedc917816bffad6bb77ab7292d2d4394391d
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-01 11:09:12 +0000

    Update prowler_script.js
    Implemented table loading

--- scripts/prowler_script.js
+++ scripts/prowler_script.js
@@ -1,191 +1,184 @@
-// initialiser function to get content <- This is SUPER hacky and should not be done, find a better way to do this
-function get_file(gots, gets) {
-  // check if all are got
-  if (gets.length === 0) {
-    // call main with list of file content (reverse so the order corrosponds with gets)
-    main(gots.reverse())
+// debug log
+console.log('Prowler: Loaded Prowler')
+// initialise match frequency, matches list, match_count, state and waiting
+let match_freq = {};
+let matches = [];
+let match_count = 0;
+let state = "prowling";
+let waiting = true;
+// get bad words
+let bad_words = [];
+// get warning head and body
+let warning_head = "";
+let warning_body = "";
+// add chrome message listener
+chrome.runtime.onMessage.addListener(scan);
+
+// functions
+
+// check given string for mathces with bad_words
+function string_check(text) {
+  // check for undefined value
+  if (text === undefined) {
+    // word cannot be in undefined
+    return false
   } else {
-    // get chrome extension path of URL
-    url = chrome.runtime.getURL(gets.pop());
-    // initialise resource fetcher
-    let fetcher = new XMLHttpRequest();
-    // when resource has fetched
-    fetcher.onreadystatechange = function() {
-      if (fetcher.readyState == 4 && fetcher.status == 200) {
-        // add content to gets
-        gots.push(fetcher.responseText);
-        // recurse
-        get_file(gots, gets)
-      }
-    };
-    // fetch resource
-    fetcher.open("GET", url, true);
-    fetcher.send();
-  }
-};
-
-function main(files) {
-  // debug log
-  console.log('Prowler: Loaded Prowler')
-  // initialise matches list and match_count
-  let matches = [];
-  let match_count = 0;
-  
-  // functions
-  
-  // check given string for mathces with bad_words
-  function string_check(text) {
-    // check for undefined value
-    if (text === undefined) {
-      // word cannot be in undefined
-      return false
-    } else {
-      // initialise hit flag
-      hit = false;
-      for (let i = 0; i < bad_words.length; i++) {
-        // search for a match
-        if (text.toUpperCase().includes(bad_words[i].toUpperCase())) {
-          // set hit and increment match_count
-          hit = true;
-          match_count++
-        }
-      };
-      // return if there has been a match
-      return hit
-    }
-  };
-  // recursive function to traverse DOM and get to end nodes
-  function traverse(element){
-    // end of tree
-    if (element.childElementCount === 0) {
-      // search alt if element is img
-      if (element.tagName === 'IMG') {
-        if (string_check(element.alt)) {
-          matches.push(element);
-          // log the finding
-          //console.log('Prowler: Found critter in', element)
-        }
-      // search innerText if innerText is not blank
-      } else if (element.innerText !== '') {
-        if (string_check(element.innerText)) {
-          matches.push(element);
-          // log the finding
-          //console.log('Prowler: Found critter in', element)
-        }
-      }
-    // recurse funcrion on all elements
-    } else {
-      for (let i = 0; i < element.childElementCount; i++) {
-        traverse(element.children[i])
-      }
-    }
-  };
-  // change page content to warning
-  function show_warning() {
-    // change head and body
-    document.head.innerHTML = warning_head;
-    document.body.innerHTML = warning_body;
-    // load in critter count
-    document.getElementById('critter_count').innerText = match_count;
-    // add button listeners
-    document.getElementById('continue_no_change').addEventListener('click', revert_page)
-    document.getElementById('continue_and_redact').addEventListener('click', redact_page)
-    document.getElementById('go_back').addEventListener('click', go_back)
-  };
-  // revert to original page
-  function revert_page() {
-    // change head and body
-    document.head.innerHTML = original_head;
-    document.body.innerHTML = original_body;
-  };
-  // revert to original page and redact bad words
-  function redact_page() {
-    // change head and body
-    document.head.innerHTML = original_head;
-    document.body.innerHTML = original_body;
-    // get matches again
-    traverse(document.body);
-    // redact elements
-    for (let i = 0; i < matches.length; i++) {
-      // redact ALT of images
-      if (matches[i].tagName === 'IMG') {
-        matches[i].alt = redact_string(matches[i].alt)
-      } else {
-      // redact text of element
-        matches[i].innerText = redact_string(matches[i].innerText)
-      };
-      // debug log
-      //console.log('Prowler: redacted text element', matches[i]);
-    };
-    // redact src of all images
-    images = document.getElementsByTagName('img');
-    for (let i = 0; i < images.length; i++) {
-      // redact both src and srcset
-      images[i].src = chrome.runtime.getURL('/files/black_square.png');
-      images[i].srcset = chrome.runtime.getURL('/files/black_square.png');
-      // debug log
-      //console.log('Prowler: redacted image element', images[i])
-    }
-  };
-  // redact a string of bad words
-  function redact_string(bad_string) {
-    // replace every bad word
+    // initialise hit flag
+    hit = false;
     for (let i = 0; i < bad_words.length; i++) {
-      while (bad_string.toUpperCase().includes(bad_words[i].toUpperCase())) {
-        // find bad word index
-        let start_index = bad_string.toUpperCase().indexOf(bad_words[i].toUpperCase())
-        // replace the word
-        bad_string = bad_string.substring(0, start_index) + '\u2588'.repeat(bad_words[i].length) + bad_string.substring(start_index + bad_words[i].length, bad_string.length)
+      // search for a match
+      if (text.toUpperCase().includes(bad_words[i].toUpperCase())) {
+        // set hit and increment match_count
+        hit = true;
+        match_count++;
+        // increase frequency
+        if (bad_words[i] in match_freq) {
+          match_freq[bad_words[i]]++
+        } else {
+          match_freq[bad_words[i]] = 1
+        }
       }
     };
-    // return redacted string
-    return bad_string
-  };
-  // go back a page
-  function go_back() {
-    // debug log
-    //console.log('Prowler: going back a page')
-    history.back()
-  };
-
-  // main code
-  
-  // initialise bad words and base pointer for getting words out of files[0]
-  let bad_words = []
-  let base_pointer = 0
-  // get words out of files[0]
-  for (let current_pointer = 0; current_pointer < files[0].length; current_pointer++) {
-    // check for comma
-    if (files[0][current_pointer] === ',') {
-      // append the scanned value
-      bad_words.push(files[0].substring(base_pointer, current_pointer).replace('\n', ''))
-      // update pointers
-      base_pointer = current_pointer + 1
-    }
-  };
-  // add last value if no trailing comma
-  if (!files[0].endsWith(',')) {
-    // append last value
-    bad_words.push(files[0].substring(base_pointer, files[0].length).replace('\n', ''))
-  };
-  // debug log
-  //console.log('Prowler: Bad words loaded')
-  // get original head and body
-  let original_head = document.head.innerHTML;
-  let original_body = document.body.innerHTML;
-  // get warning head and body
-  let warning_head = files[1].substring(files[1].indexOf('<head>') + 6, files[1].indexOf('</head>'));
-  let warning_body = files[1].substring(files[1].indexOf('<body>') + 6, files[1].indexOf('</body>'));
-  // scan html for bad words
-  traverse(document.body);
-  // debug log
-  //console.log('Prowler: Page traveral complete')
-  if (match_count !== 0) {
-    // display warning
-    show_warning()
+    // return if there has been a match
+    return hit
   }
 };
-
-// start the script
-files = ['files/words.csv', 'files/warning.html'];
-get_file([], files)
+// recursive function to traverse DOM and get to end nodes
+function traverse(element) {
+  // end of tree
+  if (element.childElementCount === 0) {
+    // search alt if element is img
+    if (element.tagName === 'IMG') {
+      if (string_check(element.alt)) {
+        matches.push(element);
+      }
+    // search innerText if innerText is not blank
+    } else if (element.innerText !== '') {
+      if (string_check(element.innerText)) {
+        matches.push(element);
+      }
+    }
+  // recurse funcrion on all elements
+  } else {
+    for (let i = 0; i < element.childElementCount; i++) {
+      traverse(element.children[i])
+    }
+  }
+};
+// change page content to warning
+function show_warning() {
+  // change head and body
+  document.head.innerHTML = warning_head;
+  document.body.innerHTML = warning_body;
+  // load in critter count
+  document.getElementById('critter_count').innerText = match_count;
+  // add button listeners
+  document.getElementById('table_button').addEventListener('click', show_table);
+  document.getElementById('continue_no_change').addEventListener('click', revert_page);
+  document.getElementById('continue_and_redact').addEventListener('click', redact_page);
+  document.getElementById('go_back').addEventListener('click', go_back)
+};
+// show the table
+function show_table() {
+  // get pre table
+  let element = document.getElementById('pre_table');
+  // make table rows
+  let table_content = '';
+  Object.keys(match_freq).forEach(
+    function (key) {
+      // add rows
+      table_content = table_content.concat('<tr>', '<td>', key, '</td>', '<td>', match_freq[key].toString(), '</td>', '</tr>')
+    }
+  );
+  // set to table
+  element.innerHTML = '<table>'.concat(table_content, '</table>')
+};
+// revert to original page
+function revert_page() {
+  // change head and body
+  document.head.innerHTML = original_head;
+  document.body.innerHTML = original_body;
+  // update state and waiting
+  state = "sleeping";
+  waiting = true
+};
+// revert to original page and redact bad words
+function redact_page() {
+  // change head and body
+  document.head.innerHTML = original_head;
+  document.body.innerHTML = original_body;
+  // get matches again
+  traverse(document.body);
+  // redact elements
+  for (let i = 0; i < matches.length; i++) {
+    // redact ALT of images
+    if (matches[i].tagName === 'IMG') {
+      matches[i].alt = redact_string(matches[i].alt)
+    } else {
+    // redact text of element
+      matches[i].innerText = redact_string(matches[i].innerText)
+    };
+  };
+  // redact src of all images
+  images = document.getElementsByTagName('img');
+  for (let i = 0; i < images.length; i++) {
+    // redact both src and srcset
+    images[i].src = chrome.runtime.getURL('/files/black_square.png');
+    images[i].srcset = chrome.runtime.getURL('/files/black_square.png');
+  };
+  // update state and waiting
+  state = "redacting";
+  waiting = true
+};
+// redact a string of bad words
+function redact_string(bad_string) {
+  // replace every bad word
+  for (let i = 0; i < bad_words.length; i++) {
+    while (bad_string.toUpperCase().includes(bad_words[i].toUpperCase())) {
+      // find bad word index
+      let start_index = bad_string.toUpperCase().indexOf(bad_words[i].toUpperCase())
+      // replace the word
+      bad_string = bad_string.substring(0, start_index) + '\u2588'.repeat(bad_words[i].length) + bad_string.substring(start_index + bad_words[i].length, bad_string.length)
+    }
+  };
+  // return redacted string
+  return bad_string
+};
+// go back a page
+function go_back() {
+  history.back();
+  // update waiting
+  waiting = true
+};
+// scan the page
+function scan(request, sender, respond) {
+  // debugging
+  console.log("Prowler: received");
+  // unpack request if it is for us
+  if (request.action === "prowler_scan" && waiting === true) {
+    // update variables from request
+    bad_words = request.words;
+    warning_head = request.head;
+    warning_body = request.body;
+    // get original head and body
+    window.original_head = document.head.innerHTML;
+    window.original_body = document.body.innerHTML;
+    // scan html for bad words
+    traverse(document.body);
+    // new critters detected
+    if (match_count !== 0) {
+      // update waiting
+      waiting = false;
+      // ready state
+      if (state === "prowling") {
+        // display warning
+        show_warning()
+      } else if (state === "redacting") {
+        // redact page
+        redact_page()
+      } else {
+        waiting = true
+      }
+    }
+  }
+}
