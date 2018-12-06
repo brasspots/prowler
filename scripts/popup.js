7e1f3e1eb51ae7b8508f81dc83c72ae585193d4e
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-06 15:22:54 +0000

    Update popup.js
    Added valid action to request
    set toggles onclick to comply with CSP

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,6 @@
 // send toggle updates to background script
 function send_update() {
-  chrome.runtime.sendMessage({w_scan: document.getElementById('scan_word_toggle').checked, i_scan: document.getElementById('scan_img_toggle').checked, w_redact: document.getElementById('redact_word_toggle').checked, i_redact: document.getElementById('redact_img_toggle').checked}, function (resonce) {});
+  chrome.runtime.sendMessage({action: "prowler_popup_set", w_scan: document.getElementById('scan_word_toggle').checked, i_scan: document.getElementById('scan_img_toggle').checked, w_redact: document.getElementById('redact_word_toggle').checked, i_redact: document.getElementById('redact_img_toggle').checked}, function (resonce) {});
 };
 
 // set all toggles to checked
@@ -9,7 +9,10 @@ let toggles = document.getElementsByTagName('input');
 for (let i = 0; i < toggles.length; i++) {
   // assert checkbox
   if (toggles[i].type === "checkbox") {
-    // set checked
-    toggles[i].checked = true
+    // set checked and onclick
+    toggles[i].checked = true;
+    toggles[i].onclick = send_update
   }
-}
+};
+// update
+send_update()
