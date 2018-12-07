2efc9f4b77311b6863ed1e494af762b9e5a05ec7
brasspots <44511851+brasspots@users.noreply.github.com>
2018-12-07 08:23:58 +0000

    Update popup.js
    Moved main body into response function to circumvent syncronization issues

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,35 +2,26 @@
 function send_update() {
   chrome.runtime.sendMessage({action: "prowler_popup_set", w_scan: document.getElementById('scan_word_toggle').checked, i_scan: document.getElementById('scan_img_toggle').checked, w_redact: document.getElementById('redact_word_toggle').checked, i_redact: document.getElementById('redact_img_toggle').checked}, function (response) {});
 };
+
 // get toggle values from background script
 function get_update() {
   // send request
   chrome.runtime.sendMessage({action: "prowler_popup_get"}, function (response) {
-    // set toggle array values
-    toggle_values[0] = response.scan_word;
-    toggle_values[1] = response.scan_img;
-    toggle_values[2] = response.redact_word;
-    toggle_values[3] = response.redact_img;
-    // set got flag
-    got = true;
-    console.log(toggle_values)
-  });
-  console.log(toggle_values)
+    // initialise toggle state array from background and got flag
+    let toggle_values = [response.scan_word, response.scan_img, response.redact_word, response.redact_img];
+    // set all toggles to their state from background
+    let toggles = document.getElementsByTagName('input');
+    // iterate over toggles
+    for (let i = 0; i < toggles.length; i++) {
+      // assert checkbox
+      if (toggles[i].type === "checkbox") {
+        // set checked and onclick
+        toggles[i].checked = toggle_values[i];
+        toggles[i].onclick = send_update;
+      }
+    }
+  })
 };
-// initialise toggle state array from background and got flag
-let toggle_values = [];
-let got = false;
-console.log(toggle_values);
+
+// get update to load initial values
 get_update();
-console.log(toggle_values);
-// set all toggles to their state from background
-let toggles = document.getElementsByTagName('input');
-// iterate over toggles
-for (let i = 0; i < toggles.length; i++) {
-  // assert checkbox
-  if (toggles[i].type === "checkbox") {
-    // set checked and onclick
-    toggles[i].checked = toggle_values[i];
-    toggles[i].onclick = send_update;
-  }
-};
